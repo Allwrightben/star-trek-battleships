5240cc22a60e2d6ffcde8dd3096c7e04f4a888b0
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-02-03 14:58:10 +0000

    Add boards, convert letters to numbers and create needed functions

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+HIDDEN_BOARD = [[' '] * 8 for x in range (8)]
+GUESS_BOARD = [[' '] * 8 for x in range (8)]
+
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+}
+
+
+def board():
+    pass
+
+def create_starships():
+    pass
+
+def get_starship_location():
+    pass
+
+def starship_hit_count():
+    pass
+
+
+
