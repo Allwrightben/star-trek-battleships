140eb997ee898c0bd8060cde57566153733ef0bc
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-02-03 15:39:18 +0000

    Complete create_startships function

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# X for starship placement and hit
+# ' ' for available space
+# '-' for missed shot
+
+from random import randint
 
 
+# Create boards
 HIDDEN_BOARD = [[' '] * 8 for x in range (8)]
 GUESS_BOARD = [[' '] * 8 for x in range (8)]
 
+# Convert letters to numbers for traditional battleships style
 letters_to_numbers = {
     'A': 0,
     'B': 1,
@@ -17,18 +21,35 @@ letters_to_numbers = {
     'H': 7
 }
 
+# Print boards to terminal
+def print_board(board):
+    print('    A B C D E F G H')
+    print('    ---------------')
+    row_num = 1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num += 1
 
-def board():
-    pass
+# Create 5 starships in random locations
+def create_starships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,7), randint(0,7)
+        while board[ship_row] [ship_column] == 'X':
+            ship_row, ship_column = randint(0,7), randint(0,7)
+        board[ship_row] [ship_column] = 'X'
 
-def create_starships():
-    pass
+
+    
 
 def get_starship_location():
     pass
 
 def starship_hit_count():
     pass
+
+create_starships()
+turns = 10
+while turns > 0:
 
 
 
